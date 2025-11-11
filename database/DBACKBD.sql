80ee59c4f287437cd6b8551bee059c914544e7aa
Angek <angek@example.com>
2025-11-11 02:47:04 -0700

    SQL: remover DEFINER de trigger guardar_fecha_hora_mantenimiento para compatibilidad en hosting

--- database/DBACKBD.sql
+++ database/DBACKBD.sql
@@ -94,7 +94,7 @@ UNLOCK TABLES;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 /*!50003 CREATE*/ /*!50003 TRIGGER `ActualizarFechaIngresoEmpleado` BEFORE INSERT ON `empleados` FOR EACH ROW BEGIN
-    SET NEW.Fecha_Ingreso = CURRENT_DATE();
+        SET NEW.Fecha_Ingreso = CURRENT_DATE();
 END */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -233,7 +233,7 @@ UNLOCK TABLES;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `guardar_fecha_hora_mantenimiento` BEFORE INSERT ON `mantenimiento_gruas` FOR EACH ROW BEGIN
+/*!50003 CREATE*/ /*!50003 TRIGGER `guardar_fecha_hora_mantenimiento` BEFORE INSERT ON `mantenimiento_gruas` FOR EACH ROW BEGIN
     SET NEW.Fecha_Hora = NOW();
 END */;;
 DELIMITER ;
